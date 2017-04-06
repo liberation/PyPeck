2c3b2eeb166b688ffcc9f084b85d23973c309755
Nahuel Angelinetti <n.angelinetti@liberation.fr>
2017-04-06 17:19:56 +0200

    Use facebooksdk library and fix tests

--- pypeck/scrapper/facebook.py
+++ pypeck/scrapper/facebook.py
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
 import re
 
+from pypeck.datetools import parse_datetime
 from pypeck.scrapper import Scrapper
 from pypeck.scrapper.constants import ASSET_TYPES
+
+import facebook
 
 
 class FacebookScrapper(Scrapper):
@@ -20,8 +26,32 @@ class FacebookScrapper(Scrapper):
     def is_site_specific(cls):
         return True
 
+    def __init__(self, *args, **kwargs):
+        super(FacebookScrapper, self).__init__(*args, **kwargs)
+        self.api_data = {}
+
+        self.API = facebook.GraphAPI(
+            access_token=self.config.get('facebook', {}).get('TOKEN', None),
+            version='2.7')
+
+    def extract_metas(self):
+        self.extract_og_metas()
+        self.extract_provider_content()
+
+    def get_post_id(self):
+        username, post_type, u, post_id = re.findall(
+            r'facebook.com/([a-zA-Z0-9\\.\\-]+)/'
+            r'(posts|activity|photos|videos)/([^/]*/)?(\d{10,})',
+            self.get_url())[0]
+
+        user_id = self.API.get_object(username).get('id')
+        return "{}_{}".format(user_id, post_id)
+
     def extract_provider_content(self):
-        pass
+        self.api_data = self.API.get_object(
+            self.get_post_id(),
+            fields='created_time,from,message,name,picture,type,source'
+        )
 
     def get_id(self):
         return re.findall('(\d+)/?$', self.get_url())[0]
@@ -30,22 +60,32 @@ class FacebookScrapper(Scrapper):
         return self.og.get('og:url')
 
     def get_content(self):
-        return self.og.get('og:description')
+        return self.api_data.get('message')
 
     def get_image(self):
-        return self.og.get('og:image')
+        return self.api_data.get('picture', None)
+
+    def get_video(self):
+        return self.api_data.get('source', None)
 
     def get_user(self):
         return {
-            'username': self.oembed.get('author_name'),
-            'url': self.oembed.get('author_url'),
+            'username': self.api_data.get('from').get('name'),
+            'url': u'https://www.facebook.com/{}'.format(
+                self.api_data.get('from').get('id')
+            ),
         }
+
+    def get_date(self):
+        return parse_datetime(self.api_data.get('created_time'))
 
     def get_datas(self):
         return {
             'id': self.get_id(),
             'url': self.get_url(),
+            'date': self.get_date(),
             'content': self.get_content(),
             'image': self.get_image(),
+            'video': self.get_video(),
             'user': self.get_user(),
         }
--- tests/test_facebook.py
+++ tests/test_facebook.py
@@ -1,33 +1,100 @@
 # -*- coding: utf-8 -*-
 import unittest
+import os
+from datetime import datetime
 from pypeck.scrapper import Scrapper
 from pypeck.scrapper.facebook import FacebookScrapper
 
 
 class FacebookScrapperTest(unittest.TestCase):
+    def setUp(self):
+        self.config = {
+            'facebook': {
+                'TOKEN':
+                    os.environ.get('FB_TOKEN'),
+            },
+        }
+
     def test_scrap_content(self):
         scrapper = Scrapper.get_scrapper_for_url(
             'https://www.facebook.com/Liberation/posts/10155040633377394',
+            self.config
         )
         self.assertIs(scrapper.__class__, FacebookScrapper)
 
         scrapper.extract_metas()
 
         datas = scrapper.get_datas()
+        self.assertIsNotNone(datas.get('content'))
+        self.assertIsNone(datas.get('video'))
+        self.assertIsNotNone(datas.get('image'))
+        self.assertIsInstance(datas.get('date'), datetime)
         self.assertEqual(
-            datas.keys(),
-            ['url', 'content', 'image', 'id', 'user']
+            datas.get('url'),
+            'https://www.facebook.com/Liberation/posts/10155040633377394'
+        )
+        self.assertEqual(datas.get('user'), {
+            'url': u'https://www.facebook.com/147126052393',
+            'username': u'Libération'}
+         )
+
+
+        scrapper = Scrapper.get_scrapper_for_url(
+            'https://www.facebook.com/BFMTV/videos/10155428810852784/',
+            self.config
+        )
+        self.assertIs(scrapper.__class__, FacebookScrapper)
+
+        scrapper.extract_metas()
+
+        datas = scrapper.get_datas()
+        self.assertIsNotNone(datas.get('content'))
+        self.assertIsNotNone(datas.get('video'))
+        self.assertIsNotNone(datas.get('image'))
+        self.assertIsInstance(datas.get('date'), datetime)
+        self.assertEqual(
+            datas.get('url'),
+            'https://www.facebook.com/BFMTV/videos/10155428810852784/'
         )
 
         scrapper = Scrapper.get_scrapper_for_url(
-            'https://www.facebook.com/BFMTV/videos/10155428810852784/'
+            'https://www.facebook.com/Liberation/videos/'
+            'vb.147126052393/10155019316047394/?type=2&theater',
+            self.config
         )
         self.assertIs(scrapper.__class__, FacebookScrapper)
 
         scrapper.extract_metas()
 
         datas = scrapper.get_datas()
+
+        self.assertIsNotNone(datas.get('content'))
+        self.assertIsNotNone(datas.get('video'))
+        self.assertIsNotNone(datas.get('image'))
+        self.assertIsInstance(datas.get('date'), datetime)
         self.assertEqual(
-            datas.keys(),
-            ['url', 'content', 'image', 'id', 'user']
+            datas.get('url'),
+            'https://www.facebook.com/Liberation/videos/10155019316047394/'
+        )
+
+        scrapper = Scrapper.get_scrapper_for_url(
+            'https://www.facebook.com/Liberation/photos/'
+            'a.329987802393.196415.147126052393/10155085314147394/'
+            '?type=3&permPage=1',
+            self.config
+        )
+        self.assertIs(scrapper.__class__, FacebookScrapper)
+
+        scrapper.extract_metas()
+
+        datas = scrapper.get_datas()
+
+        self.assertIsNotNone(datas.get('content'))
+        self.assertIsNone(datas.get('video'))
+        self.assertIsNotNone(datas.get('image'))
+        self.assertIsInstance(datas.get('date'), datetime)
+        self.assertEqual(
+            datas.get('url'),
+            'https://www.facebook.com/Liberation/photos/'
+            'a.329987802393.196415.147126052393/10155085314147394/?type=3'
         )
