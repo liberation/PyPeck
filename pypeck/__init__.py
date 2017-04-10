1a6670054eb3956db350597d62ee66642a8438ea
Nahuel Angelinetti <n.angelinetti@liberation.fr>
2017-04-10 11:33:15 +0200

    Fix init imports

--- pypeck/__init__.py
+++ pypeck/__init__.py
@@ -1,13 +1,15 @@
-from .scrapper import Scrapper
+from pypeck.scrapper import Scrapper
 
 
 class PyPeck(object):
-    def __init__(self, url):
+    def __init__(self, url, config):
         self.url = url
-        self.scrapper = Scrapper.get_scrapper_for_url(url)
+        self.config = config
+        self.scrapper = Scrapper.get_scrapper_for_url(url, config)
 
     def process(self):
         self.scrapper.extract_metas()
 
     def get_datas(self):
         return self.scrapper.get_datas()
+
